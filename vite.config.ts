005dfd430ffdfff13f3710cdeaefb99169fd6e25
ynscftclr <yunus.ciftciler@atmosware.turkcell.com.tr>
2026-05-14 16:50:38 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,9 @@ import mockApiPlugin from "./mock/mockApiPlugin.js";
 
 export default defineConfig({
   plugins: [react(), mockApiPlugin()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+  server: {
+    host: true,
+    allowedHosts: "all",
   },
   preview: {
     host: true,
